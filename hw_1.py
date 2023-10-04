063297fd244b5b980ef9cdd493c0e58db5f68bdc
Beyshenali Imatov <beyshenali.imatov.kk@gmail.com>
2023-10-04 18:05:10 +0600

    first commit

---
+++ hw_1.py
@@ -0,0 +1,19 @@
+# Задание 1:
+# Создайте класс Fruits c атрибутами - (name, color, weight)
+# Создайте три объекта класса и с помощью метода выведите информацию о каждом фрукте
+
+
+# class Fruit:
+#     def __init__(self, name, color, weight):
+#         self.name = name
+#         self.color = color
+#         self.weight = weight
+
+#     def info(self):
+#         print(
+#             f"Сорт яблока - {self.name}, цвет - {self.color}, масса - {self.weight} кг"
+#         )
+
+
+# apple = Fruit("Jannat", "red", 0.25)
+# apple.info()
